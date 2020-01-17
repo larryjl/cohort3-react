11297b0f711fa9e8989ffe9f6b2a970559de554b
larryjl <leelawrencej@gmail.com>
2020-01-17 09:44:26 -0700

    Updates

---
+++ precache-manifest.c303d3c810599f9896db4cb1b61128be.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b539927a6bd84591426ee0316c26f3f",
+    "url": "/cohort3-react/index.html"
+  },
+  {
+    "revision": "7241b32b85ebf01cf18d",
+    "url": "/cohort3-react/static/css/main.b3e28e6b.chunk.css"
+  },
+  {
+    "revision": "95e059481ef98910e04d",
+    "url": "/cohort3-react/static/js/2.a7011e00.chunk.js"
+  },
+  {
+    "revision": "7241b32b85ebf01cf18d",
+    "url": "/cohort3-react/static/js/main.28a7680c.chunk.js"
+  },
+  {
+    "revision": "56dcee979c2a984ca555",
+    "url": "/cohort3-react/static/js/runtime-main.3d2166cf.js"
+  },
+  {
+    "revision": "2939420d8dc3b29d6872804869512f9b",
+    "url": "/cohort3-react/static/media/Icon_attention_circle.2939420d.svg"
+  },
+  {
+    "revision": "f9b16baa76c87dd03dfa8162261853a7",
+    "url": "/cohort3-react/static/media/Icon_card.f9b16baa.svg"
+  },
+  {
+    "revision": "ca016d05ad5baa5636fe41bdf8da2b9b",
+    "url": "/cohort3-react/static/media/Icon_check.ca016d05.svg"
+  },
+  {
+    "revision": "17cbe1bcb140c730d4fcec4a47b0248c",
+    "url": "/cohort3-react/static/media/Icon_close_circle.17cbe1bc.svg"
+  },
+  {
+    "revision": "fe5fa6d3df62e9e7f6471184ae42e010",
+    "url": "/cohort3-react/static/media/Icon_left.fe5fa6d3.svg"
+  },
+  {
+    "revision": "0d9d6bbca79bcc4f24145a70327f2ec3",
+    "url": "/cohort3-react/static/media/Icon_link.0d9d6bbc.svg"
+  },
+  {
+    "revision": "db1c408a3cf352331030bc5ba95460b3",
+    "url": "/cohort3-react/static/media/Icon_play.db1c408a.svg"
+  },
+  {
+    "revision": "0674e5f165293ac7890aa5bcf44844b2",
+    "url": "/cohort3-react/static/media/Icon_record.0674e5f1.svg"
+  },
+  {
+    "revision": "6b6f5bf170d9ab32aedae29da6ec7479",
+    "url": "/cohort3-react/static/media/Icon_right.6b6f5bf1.svg"
+  },
+  {
+    "revision": "cd8d09a375f2822a367685c49f5ff18d",
+    "url": "/cohort3-react/static/media/Icon_sorting.cd8d09a3.svg"
+  },
+  {
+    "revision": "37b324894fafc3670e14a3dd7ec0a6b1",
+    "url": "/cohort3-react/static/media/Icon_up.37b32489.svg"
+  },
+  {
+    "revision": "92c09fb0195a0f55c065bf91ea78d552",
+    "url": "/cohort3-react/static/media/Icon_user_circle.92c09fb0.svg"
+  },
+  {
+    "revision": "7663f3eec364ac0f7a3edd4c0b82f130",
+    "url": "/cohort3-react/static/media/Icon_view.7663f3ee.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/cohort3-react/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aa75c0455e8788a2d07284b0bdeb489b.js"
+  "/cohort3-react/precache-manifest.c303d3c810599f9896db4cb1b61128be.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cohort3-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
